c39c53adf90e09a22b81591f4c557b9abcfee3fd
Cameron Taylor <camerontaylor00@gmail.com>
2014-08-23 23:37:01 -0700

    Split for code reuse

--- wikigame.py
+++ wikigame.py
@@ -3,36 +3,8 @@ Created on Aug 7, 2014
 
 @author: Cameron Taylor
 '''
+import wikipage
 
-import urllib2
-from bs4 import BeautifulSoup
-
-class WikiPage(object):
-    """
-    This object is a given wiki page. It contains the links
-    contained in its page
-    """
-    links = {}
-    url = ""
-    name = ""
-    def __init__(self, name):
-        self.name = name.replace(" ", "_")
-        self.url = "http://en.wikipedia.org/wiki/{}".format(self.name)
-        self.get_links(self.url)
-    def get_links(self, url):
-	"""
-	Scrapes Wiki page for links contained
-	"""
-        link_dic = {}
-        response = urllib2.urlopen(url)
-        soup = BeautifulSoup(response.read())
-        for element in soup.select("p a"):
-            if element.has_attr('href') and element.has_attr('title'):
-                title = element['title']
-                wiki = element['href']
-                wiki = wiki.replace("/wiki/", "")
-                link_dic[title] = wiki
-        self.links = link_dic
 class WikiGame(object): 
     """
     A game object for wikilink.
